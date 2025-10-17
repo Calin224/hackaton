b3f3dc01d71e3cd2d6bb83b42f544460136e94a8
calin <sanducalinm@gmail.com>
2025-10-17 18:53:46 +0300

    added login

---
+++ Backend/API/Extensions/ClaimsPrincipleExtensions.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Security.Authentication;
+using System.Security.Claims;
+using Core.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions;
+
+
+public static class ClaimsPrincipleExtensions
+{
+    public static async Task<AppUser> GetUserByEmail(this UserManager<AppUser> userManager, ClaimsPrincipal user)
+    {
+        var userToReturn = await userManager.Users.FirstOrDefaultAsync(x => x.Email == user.GetEmail());
+        return userToReturn ?? throw new AuthenticationException("User not found!");
+    }
+    public static string GetEmail(this ClaimsPrincipal user)
+    {
+        var email = user.FindFirstValue(ClaimTypes.Email);
+        return email ?? throw new AuthenticationException("Email claim not found!");
+    }
+}
+
--- Backend/API/Program.cs
+++ Backend/API/Program.cs
@@ -1,23 +1,56 @@
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddCors();
+
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<AppUser>()
+    .AddEntityFrameworkStores<DataContext>();
+
+// Configure authentication to return 401 instead of redirecting for API requests
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Events.OnRedirectToLogin = context =>
+    {
+        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+        return Task.CompletedTask;
+    };
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200").AllowCredentials());
+
 app.MapControllers();
+app.MapGroup("api").MapIdentityApi<AppUser>();
+
+try
+{
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<DataContext>();
+
+    await context.Database.MigrateAsync();
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    throw;
+}
 
 app.Run();
